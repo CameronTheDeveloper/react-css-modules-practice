6bd4cb65d9c2b1fa59862a6757aa3b43988fa699
Cameron Posey <cameronposey@outlook.com>
2024-01-20 20:20:56 -0800

    Implement composition styles

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -1,7 +1,7 @@
 import styles from "./Button.module.css"
 
-export default function Button(){
+export default function Button({type="primary", label="Button"}){
     return (
-        <button className={styles.btn}>Submit</button>
+        <button className={styles[type]}>{label}</button>
     )
 }
